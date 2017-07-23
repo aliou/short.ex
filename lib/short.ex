2542a4f70b6d39ab7b21311fe94ebf3fa452bfbc
Aliou Diallo <code@aliou.me>
2017-07-23 03:31:00 +0200

    Start the InMemoryAdapter only when it is used.

--- lib/short.ex
+++ lib/short.ex
@@ -32,10 +32,12 @@ defmodule Short do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # TODO: Only add the memory adapter if it is the selected adapter.
-    children = [
-      worker(Short.InMemoryAdapter, []),
-    ]
+    # TODO: Is there a better way to do this? Also there should be a way to
+    # configure this, when using a custom adapter.
+    children = case Application.get_env(:short, :adapter) do
+      Short.InMemoryAdapter -> [worker(Short.InMemoryAdapter, [])]
+      _ -> []
+    end
 
     opts = [strategy: :one_for_one, name: Short.Supervisor]
     Supervisor.start_link(children, opts)
