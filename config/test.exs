267028e11147ae98aabfcab5432366c84d60dba3
Aliou Diallo <code@aliou.me>
2017-07-23 22:14:55 +0200

    Move back InMemoryAdapter startup in test config.

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,3 @@
 use Mix.Config
+
+config :short, :adapter, Short.Adapters.InMemoryAdapter
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,1 @@
 ExUnit.start()
-
-# Use the InMemoryAdapter for tests.
-Application.put_env(:short, :adapter, Short.Adapters.InMemoryAdapter)
-{:ok, _} = Short.Adapters.InMemoryAdapter.start_link
