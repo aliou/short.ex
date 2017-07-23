aae5bfea8cff2d5a74613301705bafdff3ed7514
Aliou Diallo <code@aliou.me>
2017-07-23 19:09:00 +0200

    Manually set the InMemory adapter in tests.

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,1 @@
 use Mix.Config
-
-config :short, :adapter, Short.InMemoryAdapter
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
 ExUnit.start()
+
+# Use the InMemoryAdapter for tests.
+Application.put_env(:short, :adapter, Short.InMemoryAdapter)
+{:ok, _} = Short.InMemoryAdapter.start_link
