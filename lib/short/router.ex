1d1935e67e67dfde9b5e408858ddf427e0abf162
Aliou Diallo <code@aliou.me>
2017-07-23 19:16:37 +0200

    Create router for short.

---
+++ lib/short/router.ex
@@ -0,0 +1,45 @@
+defmodule Short.Router do
+  @moduledoc """
+  Router for Short.
+
+  TODO: Docs about how to either forward to this from another plug, or directly
+  using this with cowboy.
+  """
+
+  use Plug.Router
+
+  plug Plug.Parsers, parsers: [:multipart], pass: ["*/*"]
+  plug :match
+  plug :dispatch
+
+  get "/:code" do
+    code = conn.params["code"]
+    case adapter().get(code) do
+      {:ok, url} -> redirect(conn, url)
+      {:error, _} -> conn |> send_resp(404, "not found")
+    end
+  end
+
+  post "/" do
+    url = conn.params["url"]
+    code = conn.params["code"]
+
+    case adapter().create(url, code) do
+      {:ok, {code, ^url}} -> conn |> send_resp(200, code)
+      {:error, error} -> conn |> send_resp(422, error.message)
+    end
+  end
+
+  match _, do: send_resp(conn, 404, "")
+
+  ## Helpers
+
+  defp adapter, do: Application.get_env(:short, :adapter)
+
+  defp redirect(conn, to) do
+    conn
+    |> put_resp_header("location", to)
+    |> resp(301, "You are being redirected.")
+    |> halt()
+  end
+end
---
+++ test/lib/short/router_test.exs
@@ -0,0 +1,63 @@
+defmodule Short.RouterTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  @options Short.Router.init([])
+
+  setup do
+    Short.InMemoryAdapter.clear!
+    :ok
+  end
+
+  describe "GET /:code" do
+    test "it returns 404 when the code doesn't exist" do
+      code = Short.Code.generate()
+      conn = conn(:get, "/#{code}") |> Short.Router.call(@options)
+
+      assert conn.status == 404
+    end
+
+    test "it redirects to the associated url when the code exists" do
+      code = Short.Code.generate()
+      url = Faker.Internet.url
+
+      adapter = Application.get_env(:short, :adapter)
+      {:ok, _} = adapter.create(url, code)
+      conn = conn(:get, "/#{code}") |> Short.Router.call(@options)
+
+      assert conn.status == 301
+      assert [url] == Plug.Conn.get_resp_header(conn, "location")
+    end
+  end
+
+  describe "POST /" do
+    test "it returns the code when shortening an URL" do
+      code = Short.Code.generate()
+      url = Faker.Internet.url
+
+      conn =
+        conn(:post, "/", %{code: code, url: url})
+        |> Plug.Conn.put_resp_header("content-type", "multipart/form-data")
+        |> Short.Router.call(@options)
+
+      assert conn.status == 200
+      assert conn.resp_body == code
+    end
+
+    test "it returns an error when there is an error" do
+      code = Short.Code.generate()
+      url = Faker.Internet.url
+
+      adapter = Application.get_env(:short, :adapter)
+      {:ok, _} = adapter.create(url, code)
+      conn =
+        conn(:post, "/", %{code: code, url: url})
+        |> Plug.Conn.put_resp_header("content-type", "multipart/form-data")
+        |> Short.Router.call(@options)
+
+      assert conn.status == 422
+      assert conn.resp_body ==
+        Short.CodeAlreadyExistsError.exception(code).message
+    end
+  end
+end
