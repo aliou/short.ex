67d17807f267c5dbceda710f99caba2c13403a34
Aliou Diallo <code@aliou.me>
2017-07-23 04:00:22 +0200

    Define application module Callback.

--- mix.exs
+++ mix.exs
@@ -15,7 +15,10 @@ defmodule Short.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [
+      mod: {Short, []},
+      extra_applications: [:logger]
+    ]
   end
 
   # Dependencies can be Hex packages:
