aa3d6550bce27d5d3a746de4562110c344fa7607
Aliou Diallo <code@aliou.me>
2017-07-24 22:46:32 +0200

    Add Short.Code.valid?/1.

--- lib/short/code.ex
+++ lib/short/code.ex
@@ -1,5 +1,10 @@
 defmodule Short.Code do
-  @moduledoc false
+  @moduledoc """
+  A Short Code.
+
+  This module defined a Short Code and the main functions for working with Short
+  Codes.
+  """
 
   @typedoc false
   @type t :: String.t
@@ -10,6 +15,26 @@ defmodule Short.Code do
     |> :crypto.strong_rand_bytes()
     |> Base.url_encode64
     |> binary_part(0, code_length())
+  end
+
+  @doc """
+  Check if the code is valid to be used in an URI.
+
+  ## Examples
+
+      iex> Short.Code.valid?("abc")
+      true
+
+      iex> Short.Code.valid?("?!&")
+      false
+
+  """
+  @spec valid?(Short.Code.t) :: Boolean
+  def valid?(code) do
+    code
+    |> to_string()
+    |> String.to_charlist()
+    |> Enum.all?(&URI.char_unreserved?/1)
   end
 
   ## Helpers
--- test/short/code_test.exs
+++ test/short/code_test.exs
@@ -1,5 +1,6 @@
 defmodule Short.CodeTest do
   use ExUnit.Case, async: true
+  doctest Short.Code
 
   describe "generate/0" do
   end
