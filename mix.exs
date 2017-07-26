8f91fad4b67678cd582247b6f7f2fa6fcbc944bb
Aliou Diallo <code@aliou.me>
2017-07-26 14:59:46 +0200

    Use Elixir 1.5.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Short.Mixfile do
     [
       app: :short,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
