5f33cfd74aa6d78a5e8316d3552dfe812d1269da
Aliou Diallo <code@aliou.me>
2017-07-26 19:24:11 +0200

    Remove environment configuration files.

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,1 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
-
-# Per environment configuration.
-import_config "#{Mix.env}.exs"
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
+Application.put_env(:short, :adapter, Short.Adapters.InMemoryAdapter)
+Application.ensure_all_started(:short)
+
 ExUnit.start()
