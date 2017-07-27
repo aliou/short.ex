44da4596f8d69d2449c9a83d47f20fa7bd6af9dd
Aliou Diallo <code@aliou.me>
2017-07-27 22:08:19 +0200

    Don't raise when the adapter is missing.

--- lib/short/application.ex
+++ lib/short/application.ex
@@ -6,11 +6,6 @@ defmodule Short.Application do
   alias Short.Adapters.InMemoryAdapter
 
   def start(_type, _args) do
-    # TODO: Is this the best place to do this?
-    if adapter_missing_in_config?() do
-      raise ArgumentError, "missing :adapter in Short application configuration."
-    end
-
     # TODO: Is there a better way to do this? Also there should be a way to
     # configure this, when using a custom adapter.
     children = case Application.get_env(:short, :adapter) do
@@ -20,9 +15,5 @@ defmodule Short.Application do
 
     opts = [strategy: :one_for_one, name: Short.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp adapter_missing_in_config? do
-    Application.get_env(:short, :adapter) == nil
   end
 end
