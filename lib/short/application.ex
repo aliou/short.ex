35b22a00b4b68bbff28622026363176d4d29a413
Aliou Diallo <code@aliou.me>
2017-07-27 22:08:35 +0200

    Always start the InMemoryAdapter with the app.

--- lib/short/application.ex
+++ lib/short/application.ex
@@ -6,12 +6,7 @@ defmodule Short.Application do
   alias Short.Adapters.InMemoryAdapter
 
   def start(_type, _args) do
-    # TODO: Is there a better way to do this? Also there should be a way to
-    # configure this, when using a custom adapter.
-    children = case Application.get_env(:short, :adapter) do
-      InMemoryAdapter -> [InMemoryAdapter]
-      _ -> []
-    end
+    children = [InMemoryAdapter]
 
     opts = [strategy: :one_for_one, name: Short.Supervisor]
     Supervisor.start_link(children, opts)
