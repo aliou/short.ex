26dcf01d009af89473d9b57437ce1060f1776b56
jose nazario <jose.monkey.org@gmail.com>
2017-08-15 14:52:37 -0400

    implement URL validation tests

--- test/short/url_test.exs
+++ test/short/url_test.exs
@@ -3,4 +3,20 @@ defmodule Short.URLTest do
 
   import Short.URL
   doctest Short.URL
+
+  test "should allow well formed URL" do
+    assert true = Short.URL.new("https://github.com/") |> Short.URL.valid?
+  end
+
+  test "should error on a non http/https URL" do
+    refute Short.URL.new("ftp://github.com/") |> Short.URL.valid?
+  end
+
+  test "should error on no scheme" do
+    refute Short.URL.new("weuiewougew") |> Short.URL.valid?
+  end
+
+  test "should error on invalid hostname" do
+    refute Short.URL.new("http://ewoweowe/") |> Short.URL.valid?
+  end
 end
