cf88391e8fe96cc38f8f7813fb913421794a7ea9
Aliou Diallo <code@aliou.me>
2017-08-15 18:05:25 +0200

    Use the Short.Adapter to handle data conversion.

--- test/short/adapters/in_memory_adapter_test.exs
+++ test/short/adapters/in_memory_adapter_test.exs
@@ -11,14 +11,14 @@ defmodule Short.Adapters.InMemoryAdapterTest do
 
   describe "get/1" do
     test "it returns an error when the code doesn't exist" do
-      code = "abc"
+      code = Short.Code.new("abc")
 
-      assert {:error, %CodeNotFoundError{code: %Short.Code{__code: ^code}}} =
+      assert {:error, %CodeNotFoundError{code: ^code}} =
         InMemoryAdapter.get(code)
     end
 
     test "it returns the code and the URL when the code exists" do
-      url = Faker.Internet.url
+      url = Faker.Internet.url |> Short.URL.new()
       code = Short.Code.generate()
 
       InMemoryAdapter.create(url, code)
@@ -29,19 +29,19 @@ defmodule Short.Adapters.InMemoryAdapterTest do
 
   describe "create/2" do
     test "it generates a code when nil is given" do
-      url = Faker.Internet.url
+      url = Faker.Internet.url |> Short.URL.new()
       assert {:ok, {_code, ^url}} = InMemoryAdapter.create(url)
     end
 
     test "it uses the given code when available" do
-      url = Faker.Internet.url
+      url = Faker.Internet.url |> Short.URL.new()
       code = Short.Code.new("my-custom-code")
 
       assert {:ok, {^code, ^url}} = InMemoryAdapter.create(url, code)
     end
 
     test "it returns an error when the code already exists" do
-      url = Faker.Internet.url
+      url = Faker.Internet.url |> Short.URL.new()
       code = Short.Code.new("my-custom-code")
 
       InMemoryAdapter.create(url, code)
@@ -51,7 +51,7 @@ defmodule Short.Adapters.InMemoryAdapterTest do
     end
 
     test "it returns the existing code if the URL already exists" do
-      url = Faker.Internet.url
+      url = Faker.Internet.url |> Short.URL.new()
       code = Short.Code.new("my-custom-code")
 
       InMemoryAdapter.create(url, code)
