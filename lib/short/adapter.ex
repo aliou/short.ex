7735df7a03c1684d37f2b172c8e27bb87115ab4e
Aliou Diallo <code@aliou.me>
2017-08-15 17:55:12 +0200

    Simplify adapter type docs.

--- lib/short/adapter.ex
+++ lib/short/adapter.ex
@@ -3,33 +3,16 @@ defmodule Short.Adapter do
   Behaviour for creating a Short adapter.
   """
 
-  @typedoc """
-  For now just a regular String. In the future, we might want to limit what this
-  type is: For example, we only want characters that are permitted in an URL to
-  be used for the code, or we might want to limit the number of characters
-  for the code.
-  """
+  @typedoc "The code representing the shortened URL"
   @type code :: Short.Code.t
 
-  @typedoc """
-  Either a regular String or a URI if we want more control.
+  @typedoc "The URL to be shortened"
+  @type url :: Short.URL.t
 
-  TODO: Check if it is neccessary to have two possible types here.
-  """
-  @type url :: String.t | URI.t
-
-  @typedoc """
-  A wrapper around a shortened URL.
-
-  TODO: Find a better name for this type.
-  """
+  @typedoc "A wrapper around a shortened URL."
   @type shortened_url :: {code, url}
 
-  @typedoc """
-  Possible errors occuring when fetching or shortening an URL.
-
-  TODO: Find a better name for this type.
-  """
+  @typedoc "Possible errors occuring when working with Short Codes."
   @type error :: Short.CodeNotFoundError.t | Short.CodeAlreadyExistsError.t
 
   @callback get(code) :: {:ok, url} | {:error, error}
