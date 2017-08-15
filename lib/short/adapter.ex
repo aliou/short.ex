c5dcdb5f932d7bd3f01b3657c101d11b55d33ee5
Aliou Diallo <code@aliou.me>
2017-08-15 18:04:25 +0200

    Add macro handling adapter boilerplate.

--- lib/short/adapter.ex
+++ lib/short/adapter.ex
@@ -17,4 +17,21 @@ defmodule Short.Adapter do
 
   @callback get(code) :: {:ok, url} | {:error, error}
   @callback create(url, code | nil) :: {:ok, shortened_url} | {:error, error}
+
+  # Experimenting with macros. For now, add some convenience functions that
+  # convert the "primitive" Elixir types into Short types.
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Short.Adapter
+      alias Short.{Code, URL}
+
+      def get(code) when is_binary(code), do: get(Code.new(code))
+
+      def create(url, code \\ nil)
+      def create(url, nil), do: create(url, Code.generate())
+      def create(url, code) when is_binary(code), do: create(url, Code.new(code))
+      def create(url, code) when is_binary(url), do: create(URL.new(url), code)
+    end
+  end
 end
