5c34879894cf1d161ab2b774021ee17e3730a72d
Aliou Diallo <code@aliou.me>
2017-09-01 06:35:56 +0200

    Add Ecto adapter.

---
+++ test/short/adapters/ecto_test.exs
@@ -0,0 +1,67 @@
+defmodule Short.Adapters.EctoAdapterTest do
+  use ExUnit.Case, async: true
+
+  alias Short.{CodeNotFoundError, CodeAlreadyExistsError}
+  alias Short.Adapters.Ecto, as: EctoAdapter
+
+  setup_all do
+    Mix.Tasks.Ecto.Create.run([])
+
+    :ok
+  end
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Short.Ecto.TestRepo)
+  end
+
+  describe "get/1" do
+    test "it returns an error when the code doesn't exist" do
+      code = "abc"
+
+      assert {:error, %CodeNotFoundError{code: %Short.Code{__code: ^code}}} =
+        EctoAdapter.get(code)
+    end
+
+    test "it returns the code and the URL when the code exists" do
+      url = Faker.Internet.url |> Short.URL.new()
+      code = Short.Code.generate()
+
+      EctoAdapter.create(url, code)
+
+      assert {:ok, ^url} = EctoAdapter.get(code)
+    end
+  end
+
+  describe "create/2" do
+    test "it generates a code when nil is given" do
+      url = Faker.Internet.url |> Short.URL.new()
+      assert {:ok, {_code, ^url}} = EctoAdapter.create(url)
+    end
+
+    test "it uses the given code when available" do
+      url = Faker.Internet.url |> Short.URL.new()
+      code = Short.Code.new("my-custom-code")
+
+      assert {:ok, {^code, ^url}} = EctoAdapter.create(url, code)
+    end
+
+    test "it returns an error when the code already exists" do
+      url = Faker.Internet.url |> Short.URL.new()
+      code = Short.Code.new("my-custom-code")
+
+      EctoAdapter.create(url, code)
+
+      assert {:error, %CodeAlreadyExistsError{code: ^code}} =
+        EctoAdapter.create(url, code)
+    end
+
+    test "it returns the existing code if the URL already exists" do
+      url = Faker.Internet.url |> Short.URL.new()
+      code = Short.Code.new("my-custom-code")
+
+      EctoAdapter.create(url, code)
+
+      assert {:ok, {^code, ^url}} = EctoAdapter.create(url)
+    end
+  end
+end
