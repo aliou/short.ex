9c364914d1702a4696d7ee89cc052d7054cc7ff1
Aliou Diallo <code@aliou.me>
2017-09-05 22:34:22 +0200

    Correctly setup ecto adapter tests.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,8 @@
+# Setup for main application tests.
 Application.put_env(:short, :adapter, Short.Adapters.InMemory)
+
+# Setup for Ecto adapter tests.
+Application.put_env(:short, :ecto_repos, [Short.Adapters.Ecto.Repo])
+Application.put_env(:short, :ecto_repo, Short.Adapters.Ecto.Repo)
 
 ExUnit.start()
