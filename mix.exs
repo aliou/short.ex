c56b9927d4191ddccbbbad67ab4a43f1fc1fcbda
Aliou Diallo <code@aliou.me>
2017-12-15 11:44:50 +0100

    short.ex 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Short.Mixfile do
   def project do
     [
       app: :short,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
